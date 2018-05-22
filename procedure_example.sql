bed44f38bbfc38be1421288f4f7d710de3fdb26a
Josip Kovačević <38467885+jkovacevic992@users.noreply.github.com>
2018-05-22 13:18:41 +0200

    Update procedure_example.sql

--- procedure_example.sql
+++ procedure_example.sql
@@ -1,9 +1,10 @@
+select * from pacijent;
 drop procedure if exists select_ic;
 delimiter $$
-create procedure select_ic()
+create procedure select_ime(IN ime_pacijenta varchar(50))
 begin
-select * from pacijent where prezime like '%ic%';
+select * from pacijent where ime_pacijenta=ime;
 end $$
 delimiter ;
 
-call select_ic;
+call select_ic('Goran');
